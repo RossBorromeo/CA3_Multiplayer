63b95de3fa70e3e0ead768b913327493d4002e70
XxJT420xX <D00238448@student.dkit.ie>
2025-05-13 20:20:37 +0100

    Camera starting to work

--- GD4RoboCatSFML/GD4RoboCatSFML-master/GD4RoboCatSFML-master/RoboCatSFMLClient/RenderManager.cpp
+++ GD4RoboCatSFML/GD4RoboCatSFML-master/GD4RoboCatSFML-master/RoboCatSFMLClient/RenderManager.cpp
@@ -1,27 +1,20 @@
 #include "RoboCatClientPCH.hpp"
 
-std::unique_ptr< RenderManager >	RenderManager::sInstance;
+std::unique_ptr<RenderManager> RenderManager::sInstance;
 
 RenderManager::RenderManager()
 {
 	view.reset(sf::FloatRect(0, 0, 1920, 1080));
-	//WindowManager::sInstance->setView(view);
+	view.zoom(0.5f);
+	mLastPlayerPosition = sf::Vector2f(0.f, 0.f); // start at origin
 
-	if (const auto& player = NetworkManagerClient::sInstance->GetGameObject(NetworkManagerClient::sInstance->GetPlayerId()))
-	{
-		view.setCenter(player->GetLocation().mX, player->GetLocation().mY);
-		WindowManager::sInstance->setView(view);
-	}
+	WindowManager::sInstance->setView(view);
 }
-
-
-
 
 void RenderManager::StaticInit()
 {
 	sInstance.reset(new RenderManager());
 }
-
 
 void RenderManager::AddComponent(SpriteComponent* inComponent)
 {
@@ -31,7 +24,6 @@ void RenderManager::AddComponent(SpriteComponent* inComponent)
 void RenderManager::RemoveComponent(SpriteComponent* inComponent)
 {
 	int index = GetComponentIndex(inComponent);
-
 	if (index != -1)
 	{
 		int lastIndex = mComponents.size() - 1;
@@ -52,20 +44,92 @@ int RenderManager::GetComponentIndex(SpriteComponent* inComponent) const
 			return i;
 		}
 	}
-
 	return -1;
 }
 
+void RenderManager::UpdateCamera()
+{
+	sf::Vector2f playerPos = FindCatCentre();
 
-//this part that renders the world is really a camera-
-//in a more detailed engine, we'd have a list of cameras, and then render manager would
-//render the cameras in order
+	if (playerPos != sf::Vector2f(-1.f, -1.f)) // Valid player found
+	{
+		constexpr float followSpeed = 0.1f;
+		const sf::Vector2f currentCenter = view.getCenter();
+		const sf::Vector2f newCenter = currentCenter + (playerPos - currentCenter) * followSpeed;
+
+		view.setCenter(newCenter);
+		WindowManager::sInstance->setView(view);
+	}
+	else
+	{
+		std::cout << " Could not find player cat — camera not updated.\n";
+	}
+}
+
+// Way of finding this clients cat, and then centre point. - Ronan
+sf::Vector2f RenderManager::FindCatCentre()
+{
+	uint32_t catID = (uint32_t)'RCAT';
+	for (auto obj : World::sInstance->GetGameObjects())
+	{
+		// Find a cat.
+		if (obj->GetClassId() == catID)
+		{
+			RoboCat* cat = dynamic_cast<RoboCat*>(obj.get());
+			auto id = cat->GetPlayerId();
+			auto ourID = NetworkManagerClient::sInstance->GetPlayerId();
+			if (id == ourID)
+			{
+				// If so grab the centre point.
+				auto centre = cat->GetLocation();
+				m_lastCatPos.x = centre.mX;
+				m_lastCatPos.y = centre.mY;
+				return sf::Vector2f(centre.mX, centre.mY);
+			}
+		}
+	}
+}
+
+
+
+
+
+void RenderManager::Render()
+{
+	UpdateCamera();
+
+	// Clear back buffer
+	WindowManager::sInstance->clear(sf::Color(100, 149, 237, 255));
+
+	// Draw background first
+	sf::Sprite bgSprite;
+	bgSprite.setTexture(*TextureManager::sInstance->GetTexture("Space"));
+	bgSprite.setPosition(0.f, 0.f);
+	WindowManager::sInstance->draw(bgSprite);
+
+	// Then draw sprites and components
+	RenderComponents();
+
+	// Draw HUD on top
+	HUD::sInstance->Render();
+
+	sf::CircleShape debugCircle(10.f);
+	debugCircle.setFillColor(sf::Color::Red);
+	debugCircle.setOrigin(10.f, 10.f); // center it
+	debugCircle.setPosition(view.getCenter());
+
+	WindowManager::sInstance->draw(debugCircle);
+
+
+	// Present to screen
+	WindowManager::sInstance->display();
+}
+
 void RenderManager::RenderComponents()
 {
-	//Get the logical viewport so we can pass this to the SpriteComponents when it's draw time
 	for (SpriteComponent* c : mComponents)
-	{	
-		WindowManager::sInstance->draw(c->GetSprite());	
+	{
+		WindowManager::sInstance->draw(c->GetSprite());
 	}
 
 	for (const auto& gameObject : World::sInstance->GetGameObjects())
@@ -74,10 +138,9 @@ void RenderManager::RenderComponents()
 		if (robocat)
 		{
 			Vector3 loc = robocat->GetLocation();
-			Vector3 textLoc = loc + Vector3(0.f, -50.f, 0.f); // 50 units above the cat
+			Vector3 textLoc = loc + Vector3(0.f, -50.f, 0.f);
 
 			int health = robocat->GetHealth();
-
 			if (health > 0)
 			{
 				std::string healthStr = StringUtils::Sprintf("HP: %d", health);
@@ -88,36 +151,12 @@ void RenderManager::RenderComponents()
 				text.setCharacterSize(20);
 				text.setFont(*FontManager::sInstance->GetFont("Pixel"));
 
-				//Center the origin of the text
-				sf::FloatRect textBounds = text.getLocalBounds();
-				text.setOrigin(textBounds.width / 2.f, textBounds.height / 2.f);
-
+				sf::FloatRect bounds = text.getLocalBounds();
+				text.setOrigin(bounds.width / 2.f, bounds.height / 2.f);
 				text.setPosition(textLoc.mX, textLoc.mY);
+
 				WindowManager::sInstance->draw(text);
 			}
 		}
 	}
 }
-
-void RenderManager::Render()
-{
-	// Clear back buffer
-	WindowManager::sInstance->clear(sf::Color(100, 149, 237, 255));
-
-	//Draw background first
-	sf::Sprite bgSprite;
-	bgSprite.setTexture(*TextureManager::sInstance->GetTexture("Space"));
-	bgSprite.setPosition(0.f, 0.f);
-	WindowManager::sInstance->draw(bgSprite);
-
-	// Then draw sprites and components
-	RenderComponents();
-
-	//Then draw HUD (always on top)
-	HUD::sInstance->Render();
-
-	// Present back buffer to screen
-	WindowManager::sInstance->display();
-}
-
-
