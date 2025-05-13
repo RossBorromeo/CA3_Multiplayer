fad049b7d8bd88b5a13549a619fe7aaaa66dfae4
RossBorromeo <91060165+RossBorromeo@users.noreply.github.com>
2025-05-13 17:45:59 +0100

    Space Background Added
    Our game now has an outer space in the background of the map.

--- GD4RoboCatSFML/GD4RoboCatSFML-master/GD4RoboCatSFML-master/RoboCatSFML/ScoreBoardManager.cpp
+++ GD4RoboCatSFML/GD4RoboCatSFML-master/GD4RoboCatSFML-master/RoboCatSFML/ScoreBoardManager.cpp
@@ -10,6 +10,14 @@ void ScoreBoardManager::StaticInit()
 
 ScoreBoardManager::ScoreBoardManager()
 {
+<<<<<<< Updated upstream
+=======
+	mDefaultColors.push_back(Colors::Black);
+	mDefaultColors.push_back(Colors::White);
+	mDefaultColors.push_back(Colors::Red);
+	mDefaultColors.push_back(Colors::Green);
+	mDefaultColors.push_back(Colors::Blue);
+>>>>>>> Stashed changes
 	mDefaultColors.push_back(Colors::LightYellow);
 	mDefaultColors.push_back(Colors::LightBlue);
 	mDefaultColors.push_back(Colors::LightPink);
